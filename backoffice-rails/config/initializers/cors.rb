8f7c7e9d3fa5bda0eb452c2828d45d5bdf808cbf
ToJasonYu <tojasonyuca@gmail.com>
2026-07-27 01:40:00 -0400

    Enable CORS for the React dashboard origin

--- backoffice-rails/config/initializers/cors.rb
+++ backoffice-rails/config/initializers/cors.rb
@@ -1,16 +1,14 @@
 # Be sure to restart your server when you modify this file.
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
+# The React dashboard runs on a different origin (port) than this API in
+# development, so the browser needs an explicit CORS allowance to call it.
 
-# Read more: https://github.com/cyu/rack-cors
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "http://localhost:5173", "http://127.0.0.1:5173"
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
